cb7cdb627df6424f1d96330221122f596b998a70
Rohan Mark Gomes <rohanmarkgomes@gmail.com>
2018-08-07 20:38:02 +0530

    Completed Assignment 1 doc

--- Assignment 1/test5.cpp
+++ Assignment 1/test5.cpp
@@ -6,22 +6,21 @@
 std::optional<int> find(const std::vector<int> &a, int n, const std::vector<int> &b, int m)
 {
     int min_index = 0, max_index = n, i, j;
-     
     while (min_index <= max_index)
     {
         i = (min_index + max_index) / 2;
         j = ((n + m + 1) / 2) - i;
-        if (i < n && j > 0 && b[j - 1] > a[i])        
+        if (i < n && j > 0 && b[j - 1] > a[i])
             min_index = i + 1;
-        else if (i > 0 && j < m && b[j] < a[i - 1])        
-            max_index = i - 1;        
+        else if (i > 0 && j < m && b[j] < a[i - 1])
+            max_index = i - 1;
         else
         {
-            if (i == 0)            
+            if (i == 0)
                 return std::optional<int>{b[j - 1]};
-            if (j == 0)            
-                return std::optional<int>{a[i - 1]};            
-            else           
+            if (j == 0)
+                return std::optional<int>{a[i - 1]};
+            else
                 return std::max(a[i - 1], b[j - 1]);
         }
     }
